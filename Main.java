7878c8fd96c23fd18f1a4bf17465beda1f7760eb
juncalserrano27 <122307470+juncalserrano27@users.noreply.github.com>
2023-03-31 23:36:42 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package examenParcial3;
+
+import java.io.IOException;
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Lector miLectorExamen = new Lector();
+		miLectorExamen.ejecutar("178054");
+	}
+}
